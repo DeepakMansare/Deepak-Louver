0a452c5ae65a35a890fb7b60f43674da41700f52
Deepak-Mansare <deepakmansare40@gmail.com>
2026-01-07 18:35:36 +0530

    empty hero and nav

--- src/components/Hero/Hero.component.tsx
+++ src/components/Hero/Hero.component.tsx
@@ -1,13 +1,3 @@
-import HeroImg from "../../assets/HeroImg.png";
-import { Navbar } from "@components";
-
 export const Hero = () => {
-  return (
-    <section
-      className="relative min-h-screen bg-cover bg-center"
-      style={{ backgroundImage: `url(${HeroImg})` }}
-    >
-      <Navbar />
-    </section>
-  );
+  return <div>Hero</div>;
 };
--- src/components/Navbar/Navbar.component.tsx
+++ src/components/Navbar/Navbar.component.tsx
@@ -1,24 +1,3 @@
 export const Navbar = () => {
-  return (
-    <nav className="absolute top-0 left-0 px-12 h-16 w-full flex items-center justify-between">
-      <div className="text-white font-bold tracking-widset">
-        LOUVER <span>SPORT</span>
-      </div>
-
-      <ul className="flex items-center justify-between">
-        <li>About us</li>
-        <li>Facilities</li>
-        <li>Membership</li>
-      </ul>
-
-      <div className="flex items-center justify-between">
-        <span>Search here..</span>
-        <img src="" alt="search icon" />
-      </div>
-
-      <div>
-        <button>Book now</button>
-      </div>
-    </nav>
-  );
+  return <div>Navbar</div>;
 };
