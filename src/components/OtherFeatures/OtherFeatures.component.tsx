bcdc2749ad2c93c097b09ee7c84d5a48adb51d0a
Deepak-Mansare <deepakmansare40@gmail.com>
2026-01-25 21:55:34 +0530

    layout update for perfectpixel

--- src/components/OtherFeatures/OtherFeatures.component.tsx
+++ src/components/OtherFeatures/OtherFeatures.component.tsx
@@ -2,48 +2,48 @@ import { ArrowUpRight } from "lucide-react";
 
 export const OtherFeatures = () => {
   return (
-    <section className="flex flex-col space-y-5 font-roboto px-3 md:flex-row md:space-y-0 md:gap-6 md:space-x-3 md:px-7 md:h-96">
-      <div className="inline-flex flex-col space-y-6 md:h-96 md:w-1/3 md:space-y-4">
-        <div className="flex items-center space-x-2 text-xs md:flex-nowrap md:whitespace-nowrap">
-          <button className="px-3 py-2 border border-gray-300 rounded-3xl whitespace-nowrap">
-            Competition
-          </button>
-          <button className="px-3 py-2 border border-gray-300 rounded-3xl whitespace-nowrap">
-            Training
-          </button>
-          <button className="px-3 py-2 border border-gray-300 rounded-3xl whitespace-nowrap">
-            Friendly Match
-          </button>
+    <section className="flex px-16">
+      <div className="flex flex-col items-center justify-around space-y-4 md:flex-row">
+        <div className="flex flex-col items-start justify-around space-y-6 md:w-2/5 md:h-full">
+          <div className="flex text-sm items-start justify-center space-x-3">
+            <button className="border px-4 py-3 rounded-full shrink-0">
+              Competition
+            </button>
+            <button className="border px-4 py-3 rounded-full shrink-0">
+              Training
+            </button>
+            <button className="border px-4 py-3 rounded-full shrink-0">
+              Friendly Match
+            </button>
+          </div>
+          <div className="text-3xl md:text-6xl font-thin md:max-w-lg">
+            <p>Experience the best in Courts, Fields, and More</p>
+          </div>
+          <div className="text-sm md:max-w-80">
+            <p>
+              Our state-of-art sports facilities offer a variety of courts and
+              fields, including tennis, basketball and football, designed to
+              cater to both casual player and competitive athletes.
+            </p>
+          </div>
+          <div className="flex text-xs items-center justify-around border px-5 py-3 rounded-full space-x-2 bg-black text-white">
+            <button>Book now</button>
+            <ArrowUpRight size={16} />
+          </div>
         </div>
 
-        <h1 className="text-5xl font-thin md:font-light">
-          Experience the best in Courts, Fields, and More
-        </h1>
-
-        <p className="text-sm md:text-sm font-thin">
-          Our state-of-art sports facilities offer a variety of courts and
-          fields, including tennis, basketball and football, designed to cater
-          to both casual player and competitive athletes.
-        </p>
-
-        <button className="bg-black text-white inline-flex w-fit items-center px-4 py-2 rounded-3xl space-x-1 text-xs">
-          <span>Book Now</span>
-          <span>
-            <ArrowUpRight size={14} />
-          </span>
-        </button>
-      </div>
-
-      <div className="rounded-3xl overflow-hidden max-w-3xl mx-auto md:max-w-none md:w-2/3 md:h-96">
-        <img
-          src="/OtherFeatureImg2.png"
-          className="w-full h-full object-cover md:hidden "
-        />
-
-        <img
-          src="/OtherFeatureImg1.png"
-          className="hidden md:block w-full h-full object-cover"
-        />
+        <div className="md:w-4/7 md:h-full">
+          <img
+            src="./OtherFeatureImg1.png"
+            alt=""
+            className="md:block hidden object-cover"
+          />
+          <img
+            src="./OtherFeatureImg2.png"
+            alt=""
+            className="md:hidden block"
+          />
+        </div>
       </div>
     </section>
   );
