e80edacf27808fbef17d9e173ff1ef4a8ba568a4
Deepak-Mansare <deepakmansare40@gmail.com>
2026-01-26 17:07:15 +0530

    Newsletter update perfectPixel

--- src/components/Newsletter/Newsletter.component.tsx
+++ src/components/Newsletter/Newsletter.component.tsx
@@ -7,8 +7,8 @@ export const Newsletter = () => {
   const [email, setEmail] = useState("");
 
   return (
-    <section className="px-4">
-      <div className="flex flex-col bg-gray-900 rounded-3xl p-4 text-white font-roboto space-y-10 md:grid md:grid-cols-3 md:gap-8">
+    <section className="px-2 py-4 md:px-16 md:py-14">
+      <div className="flex flex-col bg-gray-900 rounded-3xl p-4 text-white font-roboto space-y-14 md:space-y-4 md:space-x-0 md:grid md:grid-cols-3 md:gap-8">
         <div className="w-full h-full">
           <img
             src={NewsletterImg}
@@ -22,17 +22,17 @@ export const Newsletter = () => {
             <button className="border border-white/40 rounded-3xl px-4 py-3">
               Membership
             </button>
-            <button className="bg-white text-black rounded-3xl px-4 py-3">
+            <button className="bg-white text-black rounded-3xl px-6 py-3">
               Newsletter
             </button>
           </div>
 
-          <h1 className="text-4xl">
+          <h1 className="text-3xl md:text-4xl leading-snug">
             Get the Last Updates, Special Offers, and Exclusive Event
             Invitations!
           </h1>
 
-          <div className="flex items-center w-full bg-gray-700 px-6 h-14 rounded-full max-w-lg">
+          <div className="flex items-center w-full bg-gray-700 px-6 h-16 rounded-full max-w-xl md:max-w-md">
             <input
               type="email"
               className="bg-transparent outline-none border-none w-full text-sm placeholder:text-gray-300"
