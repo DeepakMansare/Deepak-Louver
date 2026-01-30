4d6856f1572e09016ff92f4cc2dbdf8c5141702d
Deepak-Mansare <deepakmansare40@gmail.com>
2026-01-30 13:21:25 +0530

    comments resolved

--- src/components/ContactForm/ContactForm.component.tsx
+++ src/components/ContactForm/ContactForm.component.tsx
@@ -1,8 +1,23 @@
+import { inputConfig } from "@mockdata";
+import { useState } from "react";
+
 export const ContactForm = () => {
+  const [userData, setUserData] = useState<Record<string, string>>({});
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(userData);
+    setUserData({});
+  };
+
   return (
-    <section className="w-full bg-gray-50 space-y-7">
-      <div className="space-y-7 md:flex md:flex-row-reverse md:items-stretch md:w-full">
-        <div className="px-4 py-8 font-roboto font-thin md:flex md:flex-1 md:justify-center">
+    <section className="w-full h-96 bg-gray-50 space-y-7">
+      <div className="space-y-7 md:flex md:flex-row-reverse md:items-stretch md:w-full md:h-152">
+        <div className="px-4 py-8 font-roboto font-thin md:flex md:flex-1 md:justify-center md:h-full md:ml-7">
           <div className="md:flex md:flex-col space-y-4">
             <h1 className="text-4xl md:text-6xl">Get in Touch</h1>
 
@@ -10,37 +25,36 @@ export const ContactForm = () => {
               Reach out and we’ll get in touch within 24 hours.
             </p>
 
-            <form className="max-w-2xl flex flex-col space-y-4 text-xs font-medium md:w-lg md:space-y-8">
-              <input
-                placeholder="Full Name"
-                className="px-8 py-4 rounded-full border border-gray-300 md:px-10 md:py-6"
-              />
-              <input
-                placeholder="Email Address"
-                className="px-8 py-4 rounded-full border border-gray-300 md:px-10 md:py-6"
-              />
-              <input
-                placeholder="Phone Number"
-                className="px-8 py-4 rounded-full border border-gray-300 md:px-10 md:py-6"
-              />
-              <button className="px-3 py-4 rounded-full border border-gray-300 bg-cyan-200 font-medium md:px-10 md:py-5">
+            <form
+              className="max-w-2xl flex flex-col space-y-4 text-xs font-medium md:w-lg md:space-y-6 md:mt-7"
+              onSubmit={handleSubmit}
+            >
+              {inputConfig.map((item) => (
+                <input
+                  key={item.name}
+                  type={item.type}
+                  name={item.name}
+                  placeholder={item.placeholder}
+                  value={userData[item.name] || ""}
+                  onChange={handleChange}
+                  className="px-8 py-4 rounded-full border border-gray-300 md:px-10 md:py-7"
+                />
+              ))}
+              <button
+                className="px-3 py-4 rounded-full border border-gray-300 bg-sky-200 font-medium md:px-10 md:py-7 md:shadow-xl"
+                type="submit"
+              >
                 SUBMIT
               </button>
             </form>
           </div>
         </div>
 
-        <div className="max-w-2xl mx-auto md:max-w-none md:mx-0 md:flex-1 md:flex">
+        <div className="w-full h-128 md:max-w-none md:mx-0 md:flex-1 md:flex md:h-full">
           <img
-            src="/ContactForm1.png"
+            src="/ContactForm.png"
             alt="contactform"
-            className="w-full md:hidden"
-          />
-
-          <img
-            src="/ContactForm2.png"
-            alt="contactform"
-            className="hidden md:block w-full h-full object-cover"
+            className="w-full h-full object-cover"
           />
         </div>
       </div>
