8fa2d0a564e71b3c526a7e311318d66a4a1301a0
Deepak-Mansare <deepakmansare40@gmail.com>
2026-01-30 19:28:17 +0530

    minor updates in footer

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -1,12 +1,12 @@
-import { Instagram, Facebook, Linkedin, Youtube } from "lucide-react";
+import { Footerfacilities, AboutUs, Buttons } from "@mockdata";
 
 export const Footer = () => {
   return (
-    <div className="px-2">
-      <div className="flex flex-col items-center justify-center bg-sky-200 rounded-3xl font-roboto space-y-12 py-1 md:space-y-8 md:px-20 md:py-10 md:max-w-7xl md:mx-auto">
+    <div className="px-2 mb-6">
+      <div className="flex flex-col items-center justify-center bg-sky-200 rounded-3xl font-roboto space-y-12 py-1 md:space-y-8 md:px-18 md:py-10 md:max-w-full md:mx-9">
         <div className="flex flex-col space-y-8 md:flex-row md:items-start md:justify-between md:w-full">
-          <div className="flex flex-col items-start justify-center space-y-4 px-3 py-6 md:py-0">
-            <p className="text-4xl md:text-2xl md:max-w-xs">
+          <div className="flex flex-col items-start justify-center space-y-4 md:space-y-7 px-3 py-6 md:py-0">
+            <p className="text-4xl md:text-2xl md:max-w-72">
               Your Play, Your Way- Modern Sports Facilities for Every Passion
             </p>
             <button className="bg-white px-5 py-3 rounded-full text-xs">
@@ -14,65 +14,51 @@ export const Footer = () => {
             </button>
           </div>
 
-          <div className="grid grid-cols-2 px-4 md:px-2 max-w-sm mx-auto md:mx-0 md:w-full">
+          <div className="grid grid-cols-2 px-4 md:px-0 max-w-sm mx-auto md:mx-0 md:w-full md:mr-10">
             <div className="space-y-5">
               <h3 className="">Facilities</h3>
               <ul className="text-xs space-y-2">
-                <li>Tennis Courts</li>
-                <li>Basketball Courts</li>
-                <li>Football Field</li>
-                <li>Swimming Pool</li>
-                <li>Gym & Fitness Center</li>
-                <li>Multi-purpose Hall</li>
-                <li>Sauna & Relaxation Zone</li>
-                <li>Locker Rooms</li>
+                {Footerfacilities.map((item) => (
+                  <li key={item.id}>{item.label}</li>
+                ))}
               </ul>
             </div>
 
-            <div className="grid grid-cols-2 gap-3 md:gap-14">
+            <div className="grid grid-cols-2 gap-3 md:gap-18 whitespace-nowrap">
               <div className="space-y-5">
                 <h3>About Us</h3>
                 <ul className="text-xs space-y-2">
-                  <li>Our Mission</li>
-                  <li>Vision</li>
-                  <li>Community</li>
+                  {AboutUs.map((item) => (
+                    <li key={item.id}>{item.label}</li>
+                  ))}
                 </ul>
               </div>
 
-              <div className="space-y-5">
+              <div className="space-y-4">
                 <h3>Social</h3>
                 <ul className="text-xs space-y-3">
-                  <li>
-                    <button className="border border-gray-400 rounded-full px-2 py-1 flex items-center">
-                      <Instagram size={14} />
-                      instagram
-                    </button>
-                  </li>
-                  <li>
-                    <button className="border border-gray-400 rounded-full px-2 py-1 flex items-center">
-                      <Facebook size={14} />
-                      Facebook
-                    </button>
-                  </li>
-                  <li>
-                    <button className="border border-gray-400 rounded-full px-2 py-1 flex items-center">
-                      <Linkedin size={14} />
-                      linkedIn
-                    </button>
-                  </li>
-                  <li>
-                    <button className="border border-gray-400 rounded-full px-2 py-1 flex items-center">
-                      <Youtube size={14} />
-                      YouTube
-                    </button>
-                  </li>
+                  {Buttons.map((item) => (
+                    <li key={item.id}>
+                      <a
+                        href={item.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="inline-block"
+                      >
+                        <button className="border border-gray-400 rounded-full px-2 py-1 flex items-center gap-1 md:gap-3 cursor-pointer">
+                          <item.icon size={14} />
+                          {item.label}
+                        </button>
+                      </a>
+                    </li>
+                  ))}
                 </ul>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="w-full text-xs border-t border-gray-400 flex items-center justify-between px-3 py-4 md:py-6">
+        <div className="w-full text-xs border-t border-gray-400 flex items-center justify-between px-3 py-4 md:py-6 md:mt-3">
           <span>Privacy Policy/Terms</span>
           <span>All rights reserved@louver 2024</span>
         </div>
--- src/mockdata/index.ts
+++ src/mockdata/index.ts
@@ -2,3 +2,4 @@ export * from "./Navbar";
 export * from "./Hero";
 export * from "./About";
 export * from "./facilities";
+export * from "./Footer";
